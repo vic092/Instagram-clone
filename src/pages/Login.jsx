663434244f72259d5892e9bdd82478be740fd0d7
Juan Valencia <juan.vo.11.2000@gmail.com>
2022-11-13 13:06:32 -0600

    Primera parte del Login
    Se empezó a crear el login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import inicio from '../assets/inicio.png';
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <main className='container'>
+      <div className='row'>
+        <div >
+          <img src={inicio} alt="" width={400}/>
+        </div>
+        <div className='container__form colum '>
+          <h1>INSTAGRAM</h1>
+            <form action="" className='form'>
+              <input type="text" placeholder='Teléfono, usuario o correo electrónico'/>
+              <input type="text" placeholder='Contraseña'/>
+              <button className='button primary '>Iniciar sesión</button>
+            </form>
+            <hr />
+            <a href="">Iniciar sesión con Facebook</a>
+            <a href="">¿Olvidaste tu contraseña?</a>
+        </div>
+      </div>
+    </main>
   )
 }
